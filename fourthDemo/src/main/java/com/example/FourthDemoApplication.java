a6492cd0545db547bdca39cb7892532b7195a014
Kiran <kiranmai.k27@gmail.com>
2016-08-04 12:16:01 -0400

    Initial commit

---
+++ fourthDemo/src/main/java/com/example/FourthDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourthDemoApplication.class, args);
+	}
+}
